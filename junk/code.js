2901d7b185d0f560e833611b480bdf133f616884
AugustUu <98927957+AugustUu@users.noreply.github.com>
2024-10-02 20:13:42 -0700

    Update code.js

--- junk/code.js
+++ junk/code.js
@@ -1,6 +1,33 @@
 /// code.js
+console.log("loaded")
+function handlePaste(e) {
+    var clipboardData, pastedData;
 
-(function() {
-  'use strict';
-function handlePaste(e){var t,o;e.stopPropagation(),e.preventDefault(),o=(t=e.clipboardData||window.clipboardData).getData("Text");let a="query="+encodeURIComponent(o),s=new XMLHttpRequest;s.addEventListener("readystatechange",function(){this.readyState===this.DONE&&(console.log(JSON.parse(this.responseText).solution.solution.default),navigator.clipboard.writeText(JSON.parse(this.responseText).solution.solution.default),console.log(JSON.parse(this.responseText).solution.solution))}),s.open("POST","https://corsproxy.io/?"+encodeURIComponent("https://www.symbolab.com/pub_api/bridge/solution")),s.setRequestHeader("content-type","application/x-www-form-urlencoded; charset=UTF-8"),s.send(a)}console.log("loaded"),document.getElementById("content").addEventListener("paste",handlePaste),console.log("loaded frfr");
-})();
+    e.stopPropagation();
+    e.preventDefault();
+
+    clipboardData = e.clipboardData || window.clipboardData;
+    pastedData = clipboardData.getData('Text');
+
+    const data = "query=" + encodeURIComponent(pastedData);
+
+    const xhr = new XMLHttpRequest();
+    
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === this.DONE) {
+        console.log(JSON.parse(this.responseText).solution.solution.default);
+        navigator.clipboard.writeText(JSON.parse(this.responseText).solution.solution.default);
+        console.log(JSON.parse(this.responseText).solution.solution)
+      }
+    });
+    
+    xhr.open("POST", "https://corsproxy.io/?"+ encodeURIComponent("https://www.symbolab.com/pub_api/bridge/solution"));
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+    
+    xhr.send(data);
+
+}
+
+document.getElementById("content").addEventListener('paste', handlePaste);
+
+console.log("loaded frfr")
