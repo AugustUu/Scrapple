03b71070aece78356b4aca7937cfed7f9cdb2ff4
AugustUu <98927957+AugustUu@users.noreply.github.com>
2024-10-02 19:36:00 -0700

    junk

---
+++ junk/code.js
@@ -0,0 +1,29 @@
+function handlePaste(e) {
+    var clipboardData, pastedData;
+
+    e.stopPropagation();
+    e.preventDefault();
+
+    clipboardData = e.clipboardData || window.clipboardData;
+    pastedData = clipboardData.getData('Text');
+
+    const data = "query=" + encodeURIComponent(pastedData);
+
+    const xhr = new XMLHttpRequest();
+    
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === this.DONE) {
+        console.log(JSON.parse(this.responseText).solution.solution.default);
+        navigator.clipboard.writeText(JSON.parse(this.responseText).solution.solution.default);
+      }
+    });
+    
+    xhr.open("POST", "https://corsproxy.io/?"+ encodeURIComponent("https://www.symbolab.com/pub_api/bridge/solution"));
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+    
+    xhr.send(data);
+
+}
+
+document.getElementById("content").addEventListener('paste', handlePaste);
+
