fa6360e43a273aab1f6a0235b5cf4aa49d542269
AugustUu <98927957+AugustUu@users.noreply.github.com>
2024-10-03 13:35:35 -0700

    Update player.ts

--- src/player.ts
+++ src/player.ts
@@ -2,15 +2,27 @@ import { Application, Graphics } from "pixi.js";
 import { app } from ".";
 
 export class player {
-    sprite: Graphics | undefined;
+    sprite: Graphics;
 
-    constructor(x:number , y:number){
+    constructor(x: number, y: number) {
         this.sprite = new Graphics()
-        .rect(x, y, 100, 100)
-        .fill(0xff0000);
+            .rect(0, 0, 100, 100)
+            .fill(0xff0000);
+
+        this.sprite.x = x;
+        this.sprite.y = y;
 
         app.stage.addChild(this.sprite);
+
+        app.ticker.add((time) => {
+            //this.sprite.x = Math.random()*100;
+        })
+
+        this.sprite.on('globalpointermove', (event) => {
+            console.log(event)
+        })
+        this.sprite.eventMode = "static"
     }
-    
+
 
 }
