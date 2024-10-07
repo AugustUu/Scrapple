cf1aaf1c1d62cab4a81b7605a62188bfd0e73f57
tschmidt8320 <98917011+tschmidt8320@users.noreply.github.com>
2024-10-07 13:50:15 -0700

    attempted using new packages
    keyboard input works super nice and added some good wasd input
    mouse input doesnt really work and i tried to make player follow cursor but its super buggy and weird

--- src/index.ts
+++ src/index.ts
@@ -16,9 +16,27 @@ async function init(){
     const container = new Container();
     app.stage.addChild(container);
 
+
     new player(100,100);
 
     //new player(300,100+1);
+    /*const circle = app.stage.addChild(
+        new Graphics().circle(0, 0, 8).fill({ color: 0xffffff }).stroke({ color: 0x111111, alpha: 0.87, width: 1 }),
+    );
+
+    circle.position.set(app.screen.width / 2, app.screen.height / 2);
+
+    // Enable interactivity!
+    app.stage.eventMode = 'static';
+
+    // Make sure the whole canvas area is interactive, not just the circle.
+    app.stage.hitArea = app.screen;
+
+    // Follow the pointer
+    app.stage.addEventListener('pointermove', (e) =>
+    {
+        circle.position.copyFrom(e.global);
+    });*/
 }
 
 init()
