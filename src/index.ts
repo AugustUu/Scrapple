52637ac853b06a9bfacdeea23eb56d82ccf60d3e
AugustUu <98927957+AugustUu@users.noreply.github.com>
2024-10-16 23:02:37 -0700

    added (jank) physics system

--- src/index.ts
+++ src/index.ts
@@ -6,67 +6,112 @@ import { InputSystem } from './util/InputSystem';
 import { Peer } from "peerjs";
 import { MainMenu } from './ui/MainMenu';
 import { Client } from './network/client';
+import { World as World } from './world';
+import RAPIER from '@dimforge/rapier2d-compat';
 
 
 export const app = new Application();
 
-async function init(){
+async function init() {
 
-    
+
     await app.init({
         backgroundColor: 0x777777,
         resizeTo: window
     });
 
+    await RAPIER.init();
+
     document.body.appendChild(app.canvas);
-    //const container = new Container();
-    //app.stage.addChild(container);
+    const container = new Container();
+    app.stage.addChild(container);
 
 
     MainMenu.init();
 
     InputSystem.init();
-    Client.init()
+    //Client.init()
+    World.init();
 
-    StateSystem.changeSate(GameState.menu);
+    let groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.0, 1);
+    World.world.createCollider(groundColliderDesc);
+
+    let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic().setTranslation(0.0, 10.0);
+    let rigidBody = World.world.createRigidBody(rigidBodyDesc);
+
+    let colliderDesc = RAPIER.ColliderDesc.cuboid(1, 1);
+    let collider = World.world.createCollider(colliderDesc, rigidBody);
+
+    let rigidBodySprite = new Graphics().rect(100, 0, 100, 100).fill(0xff0000);
+    let groundSprite = new Graphics().rect(0, 500, 1000, 100).fill(0x0000ff);
+
+    let lines = new Graphics();
+    lines.x = window.innerWidth / 2;
+    lines.y = window.innerHeight / 2;
+
+    app.stage.addChild(lines);
+
+    app.ticker.add(() => {
+        const { vertices, colors } = World.world.debugRender();
 
 
+        lines.clear();
+
+        for (let i = 0; i < vertices.length / 4; i += 1) {
+
+            //lines.lineStyle(1.0, color, colors[i * 8 + 3], 0.5, true);
+            lines.moveTo(vertices[i * 4] *10, -vertices[i * 4 + 1] *10).lineTo(vertices[i * 4 + 2] *10, -vertices[i * 4 + 3] *10).stroke({ width: 1, color: 0xff0000 });
+
+
+        }
+
+
+    });
+
+
+    //container.addChild(rigidBodySprite)
+    //container.addChild(groundSprite)
+
+
+    //StateSystem.changeSate(GameState.menu);
+
+
+
+    /*
+        const peer = new Peer("testingtesting123");
+        const peer2 = new Peer("testingtesting1234");
+        const conn = peer.connect("testingtesting1234");
+        const conn2 = peer.connect("testingtesting123");
     
-/*
-    const peer = new Peer("testingtesting123");
-    const peer2 = new Peer("testingtesting1234");
-    const conn = peer.connect("testingtesting1234");
-    const conn2 = peer.connect("testingtesting123");
-
-    console.log(peer,conn)
-
-    conn.on("open", () => {
-        conn.send("hi!");
-    });
-
-    peer.on("connection", (conn) => {
-        conn.on("data", (data) => {
-            // Will print 'hi!'
-            console.log(data);
-        });
+        console.log(peer,conn)
+    
         conn.on("open", () => {
-            conn.send("hello!");
+            conn.send("hi!");
         });
-    });
-
-    conn2.on("open", () => {
-        conn.send("hi!");
-    });
-
-    peer2.on("connection", (conn) => {
-        conn.on("data", (data) => {
-            // Will print 'hi!'
-            console.log(data);
+    
+        peer.on("connection", (conn) => {
+            conn.on("data", (data) => {
+                // Will print 'hi!'
+                console.log(data);
+            });
+            conn.on("open", () => {
+                conn.send("hello!");
+            });
         });
-        conn.on("open", () => {
-            conn.send("hello!");
+    
+        conn2.on("open", () => {
+            conn.send("hi!");
         });
-    });*/
+    
+        peer2.on("connection", (conn) => {
+            conn.on("data", (data) => {
+                // Will print 'hi!'
+                console.log(data);
+            });
+            conn.on("open", () => {
+                conn.send("hello!");
+            });
+        });*/
 
 
     //new player(100,100);
---
+++ src/world.ts
@@ -0,0 +1,15 @@
+import RAPIER from "@dimforge/rapier2d-compat";
+import { app } from ".";
+
+export class World {
+    static gravity = { x: 0.0, y: -9.81 };
+    static world: RAPIER.World;
+
+    static init() {
+        this.world = new RAPIER.World(this.gravity);
+
+        app.ticker.add((delta) => {
+            this.world.step();
+        })
+    }
+}
