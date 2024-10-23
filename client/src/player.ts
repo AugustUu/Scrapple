a154f754e8816affa55a416138bcd6cdc08cf30e
tschmidt8320 <98917011+tschmidt8320@users.noreply.github.com>
2024-10-23 13:51:05 -0700

    made sprite actually follow player
    yay

--- client/src/player.ts
+++ client/src/player.ts
@@ -8,11 +8,11 @@ import { RigidBody } from "@dimforge/rapier2d-compat";
 
 
 
-
 export class player{
     sprite: Graphics;
     physics_object: KinematicPhysicsObject;
     rb: RigidBody;
+    window_offset: {x:number, y:number};
     constructor(x: number, y: number) {
         
         this.sprite = new Graphics()
@@ -22,8 +22,13 @@ export class player{
         this.physics_object = new KinematicPhysicsObject(0, 20, 2, 2, World, this.sprite);
         this.rb = this.physics_object.rigidBody;
 
-        this.sprite.x = this.rb.translation().x * 10;;
-        this.sprite.y = this.rb.translation().y * 10;;
+        this.sprite.x = this.rb.translation().x * 10;
+        this.sprite.y = this.rb.translation().y * 10;
+
+        this.sprite.pivot.x = this.sprite.width / 2
+        this.sprite.pivot.y = this.sprite.height / 2
+
+        this.window_offset = {x:window.innerWidth / 2, y:window.innerHeight / 2}
         
         app.stage.addChild(this.sprite);
 
@@ -31,39 +36,29 @@ export class player{
 
         this.sprite.eventMode = "static"
         this.sprite.hitArea = app.screen;
-        
-
     }
     
 
     gameLoop(delta: Ticker){
         this.play(delta);
-        this.sprite.x = this.rb.translation().x * 10 + screen.availWidth / 2 //- 20;
-        this.sprite.y = this.rb.translation().y * -10 + screen.availHeight / 2//- 62;
-
+        this.sprite.x = this.rb.translation().x * 10 + this.window_offset.x//- 20;
+        this.sprite.y = this.rb.translation().y * -10 + this.window_offset.y//- 62;
+        this.sprite.rotation = -this.rb.rotation();
     }
 
     play(delta: Ticker) {
         if(InputSystem.isKeyDown('a')){
-            this.rb.setLinvel({x: this.rb.linvel().x - 0.3, y: this.rb.linvel().y}, false);
+            this.rb.setLinvel({x:this.rb.linvel().x - 0.5, y:this.rb.linvel().y}, true);
         }
         if(InputSystem.isKeyDown('d')){
-            this.rb.setLinvel({x: this.rb.linvel().x + 0.3, y: this.rb.linvel().y}, false);
+            this.rb.setLinvel({x:this.rb.linvel().x + 0.5, y:this.rb.linvel().y}, true);
         }
         if(InputSystem.isKeyDown('s')){
-            this.rb.setLinvel({x:this.rb.linvel().x, y: -30}, false);
-            //this.rb.setTranslation({x:this.rb.translation().x, y:this.rb.translation().y - 0.5}, false);
+            this.rb.setLinvel({x:this.rb.linvel().x, y:Math.min(this.rb.linvel().y, -15)}, true);
         }
         if(InputSystem.isKeyDown('w')){
-            this.rb.setLinvel({x:this.rb.linvel().x, y:20}, true);
-            //this.rb.setTranslation({x:this.rb.translation().x, y:this.rb.translation().y + 0.5}, false);
+            this.rb.setLinvel({x:this.rb.linvel().x, y:Math.max(this.rb.linvel().y, 15)}, true);
         }
-
-        if(this.rb.linvel().x > 2){
-            this.rb.linvel().x = 2;
-        }
-
-
         if(InputSystem.isMouseDown(0)){
             var line = new Graphics();
             line.moveTo(this.sprite.x, this.sprite.y)
