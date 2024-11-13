a3217d351c725420ede3af3ed097b3818c729990
tschmidt8320 <tschmidt8320@student.pps.net>
2024-11-12 20:32:39 -0800

    lines!
    also reworked vector2 constructor yaye

--- client/src/game/Player.ts
+++ client/src/game/Player.ts
@@ -12,7 +12,8 @@ export class player {
     physics_object: KinematicPhysicsObject;
     rb: RigidBody;
     joint: ImpulseJoint;
-    line = new Graphics()
+    grapple_line = new Graphics()
+    aim_line = new Graphics()
     constructor(x: number, y: number) {
 
         this.sprite = new Graphics()
@@ -31,8 +32,8 @@ export class player {
         this.joint = World.world.createImpulseJoint(JointData.revolute({ x: 0.0, y: 0.0 }, { x: 0.0, y: 0.0 }), this.rb, this.rb, true)
         World.world.removeImpulseJoint(this.joint, true)
 
-        this.line
-        app.stage.addChild(this.line);
+        app.stage.addChild(this.grapple_line);
+        app.stage.addChild(this.aim_line);
     }
 
 
@@ -64,42 +65,55 @@ export class player {
             this.rb.setLinvel({ x: this.rb.linvel().x, y: Math.max(this.rb.linvel().y, 15) }, true);
         }
 
-        this.line.clear();
-        if (InputSystem.isMouseDown(0)) { // todo: make this only check the frame mouse is clicked, rather than every frame it is (augusts job)
-            
-            let rapierMouse = MathUtils.screenToRapier(InputSystem.getMousePos())
-            let ray = new Ray(this.rb.translation(), new Vector2(rapierMouse.x - this.rb.translation().x, rapierMouse.y - this.rb.translation().y).normalized());
-            let hit = World.world.castRay(ray, 1000, false, undefined, undefined, undefined, this.rb);
-            if (hit != null) {
-                let hitPoint = ray.pointAt(hit.timeOfImpact); 
-                console.log("Collider", hit.collider, "hit at point", hitPoint); // remove later smile
+        this.grapple_line.clear();
+        this.aim_line.clear();
+        let rapier_mouse = MathUtils.screenToRapier(InputSystem.getMousePos())
+        let ray = new Ray(this.rb.translation(), rapier_mouse.sub(this.rb.translation()).normalized());
+        let hit = World.world.castRay(ray, 1000, false, undefined, undefined, undefined, this.rb);
+        if (hit != null) {
+            let hit_point = ray.pointAt(hit.timeOfImpact); 
+            if (InputSystem.isMouseDown(0)) { // todo: make this only check the frame mouse is clicked, rather than every frame it is (augusts job)
+                
+                //console.log("Collider", hit.collider, "hit at point", hitPoint); 
                 if (!this.joint.isValid()) {
-                    this.generateJoint(hit.collider.parent(), hitPoint)
+                    this.generateJoint(hit.collider.parent(), hit_point)
                 }
-                /*if(hit.collider.parent() != null){
-                    let lineStart = MathUtils.rapierToScreen({x:this.joint.anchor1().x + this.rb.translation().x, y:this.joint.anchor1().y + this.rb.translation().y})
-                    let lineEnd = MathUtils.rapierToScreen({x:this.joint.anchor1().x + hit.collider.parent().translation().x, y:this.joint.anchor1().y + this.rb.translation().y})
-                }
-                this.line.moveTo(lineStart.x, lineStart.y).lineTo(lineEnd.x, lineEnd.y).stroke({ width: 1, color: 0x000000 })*/ // fix later ughhhhhhhhhhh how hard is it to make a line
             }
-            else {
-                console.log("miss")
+            else{
+                let line_start = MathUtils.rapierToScreen(this.rb.translation())
+                let line_end = MathUtils.rapierToScreen(hit_point)
+                this.aim_line.moveTo(line_start.x, line_start.y).lineTo(line_end.x, line_end.y).stroke({ width: 2, color: 0x000000 })
             }
         }
-        else {
-            if (this.joint != null) {
+        else{
+            let line_start = MathUtils.rapierToScreen(this.rb.translation())
+            let line_end = rapier_mouse.sub(this.rb.translation()).normalized().scale(300) // kinda stupid probably rework later
+            line_end = MathUtils.rapierToScreen(line_end.add(this.rb.translation()))
+            this.aim_line.moveTo(line_start.x, line_start.y).lineTo(line_end.x, line_end.y).stroke({ width: 2, color: 0x444444 })
+        }
+        /*else {
+            console.log("miss")
+        }*/
+
+        if(this.joint.isValid()){
+            if(!InputSystem.isMouseDown(0)){ // this feels dumb? but i can't think of another way to do it so w/e
                 World.world.removeImpulseJoint(this.joint, true)
             }
+            else{
+                let line_start = MathUtils.rapierToScreen({x:this.rb.translation().x, y:this.rb.translation().y})
+                let rot_anchor2 = new Vector2(this.joint.anchor2()).rotate(this.joint.body2().rotation())
+                let line_end = MathUtils.rapierToScreen({x:this.joint.body2().translation().x + rot_anchor2.x, y:this.joint.body2().translation().y + rot_anchor2.y})
+                this.grapple_line.moveTo(line_start.x, line_start.y).lineTo(line_end.x, line_end.y).stroke({ width: 4, color: 0x000000 })
+            }
         }
-
-
     }
 
     generateJoint(target: RigidBody | null, hitPoint: {x:number, y:number}) {
         if(target != null){ // should never be null?
-            let start_offset = new Vector2(hitPoint.x - this.rb.translation().x, hitPoint.y - this.rb.translation().y)
+            let hit_point_vector = new Vector2(hitPoint)
+            let start_offset = hit_point_vector.sub(this.rb.translation())
             start_offset = start_offset.rotate(-this.rb.rotation())
-            let end_offset = new Vector2(hitPoint.x - target.translation().x, hitPoint.y - target.translation().y)
+            let end_offset = hit_point_vector.sub(target.translation())
             end_offset = end_offset.rotate(-target.rotation())
             let params = JointData.revolute(start_offset, end_offset);
             this.joint = World.world.createImpulseJoint(params, this.rb, target, true);
