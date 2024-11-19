80e8a597de53cda9e1176dfe6298bb84541dd82d
AugustUu <98927957+AugustUu@users.noreply.github.com>
2024-11-19 13:50:48 -0800

    more netorking

--- client/src/networking/Networking.ts
+++ client/src/networking/Networking.ts
@@ -1,10 +1,10 @@
 import Colyseus, { Client, Room } from "colyseus.js";
 import { NetworkClient } from "./NetworkClient";
-import { GameEvent } from "excalibur";
-import { EventEmitter } from "stream";
+import { EventEmitter, GameEvent } from "excalibur";
 
 type NeworkEvents = {
     connected:ServerConnected
+    //couldentConnect
 }
 
 export class ServerConnected extends GameEvent<Colyseus.Room<unknown>> {
@@ -13,27 +13,29 @@ export class ServerConnected extends GameEvent<Colyseus.Room<unknown>> {
     }
 }
 
-export const NeworkEvents = {
-    connected: 'connected'
-} as const;
-
 
 export class Networking {
 
     private static colyClient: Client = new Client("ws://localhost:2567/");
     static client: NetworkClient = new NetworkClient();
     static state: any = null;
-    //static events = new EventEmitter<NeworkEvents>();
+    static events = new EventEmitter<NeworkEvents>();
 
 
     static connect(room_id: string) {
-        Networking.colyClient.joinById(room_id).then((room) => { this.client.onJoin(room) }).catch(e => {
+        Networking.colyClient.joinById(room_id).then((room) => { 
+            this.events.emit("connected", new ServerConnected(room));
+            this.client.onJoin(room) 
+        }).catch(e => {
             console.log("JOIN ERROR", e);
         });
     }
 
     static create() {
-        Networking.colyClient.create("GameRoom").then((room) => { this.client.onJoin(room) }).catch(e => {
+        Networking.colyClient.create("GameRoom").then((room) => {
+            this.events.emit("connected", new ServerConnected(room));
+            this.client.onJoin(room) 
+        }).catch(e => {
             console.log("JOIN ERROR", e);
         });
     }
