192029bd7a2dd151b61f2bfd708c9751059ca619
AugustUu <98927957+AugustUu@users.noreply.github.com>
2024-12-10 13:50:31 -0800

    added multiplayer position interpalation

--- client/src/game/OtherPlayer.ts
+++ client/src/game/OtherPlayer.ts
@@ -1,39 +1,42 @@
-import { Circle, Color, Component, CoordPlane, Entity, Font, GraphicsComponent, GraphicsGroup, Rectangle, System, SystemType, Text, TextAlign, TransformComponent, vec, Vector } from "excalibur";
+import { Circle, Color, Component, CoordPlane, Entity, Font, GraphicsComponent, GraphicsGroup, Query, Rectangle, System, SystemType, Text, TextAlign, TransformComponent, vec, Vector, World } from "excalibur";
 import { Networking } from "../networking/Networking";
 
 export class OtherPlayerComponent extends Component {
-    public name: String = 'jorbis';
+    public name: string = 'jorbis';
+    public id: string = "";
     public health: number = 100;
 
-    constructor(name: String) {
+    constructor(name: string, id: string) {
         super();
+        this.name = name;
+        this.id = id;
     }
 }
 
 
 
-export function createOtherPlayerEntity(name: string, position: Vector): Entity {
+export function createOtherPlayerEntity(name: string, id: string, position: Vector): Entity {
     const entity = new Entity({
         name: 'Player ' + name,
     });
 
-    entity.addComponent(new OtherPlayerComponent(name))
+    entity.addComponent(new OtherPlayerComponent(name, id))
 
     let sprite = new Circle({ radius: 20, color: Color.Red })
-    let nameTag = new Text({ text: name,font: new Font({  size: 30,textAlign:TextAlign.Center})})
+    let nameTag = new Text({ text: name, font: new Font({ size: 30, textAlign: TextAlign.Center }) })
 
     const group = new GraphicsGroup({
-        //useAnchor:false,
+        useAnchor:false,
         members: [
             { graphic: sprite, offset: vec(0, 0) },
-            { graphic: nameTag, offset: vec(0, -20) }
+            { graphic: nameTag, offset: vec(0, 10) }
         ]
     });
 
-
     let graphics = new GraphicsComponent();
     graphics.use(group);
-    graphics.offset = vec(0,10)
+    //graphics.offset = vec(0, 10)
+    g//raphics.anchor = vec(0.5,0.5)
 
     entity.addComponent(graphics)
 
@@ -47,3 +50,28 @@ export function createOtherPlayerEntity(name: string, position: Vector): Entity 
 }
 
 
+
+export class OtherPlayerMoveSystem extends System {
+    systemType = SystemType.Draw;
+
+    query: Query<typeof TransformComponent | typeof OtherPlayerComponent>;
+
+    constructor(world: World) {
+        super();
+        this.query = world.query([TransformComponent, OtherPlayerComponent]);
+    }
+
+    update(elapsedMs: number): void {
+
+        for (let entity of this.query.entities) {
+            let player = entity.get(OtherPlayerComponent);
+            let state = Networking.client.room!.state.players.get(player.id)
+
+            const lerp = (x: number, y: number, a: number) => x * (1 - a) + y * a;
+
+
+            entity.get(TransformComponent).pos.x = lerp(entity.get(TransformComponent).pos.x, state.x, elapsedMs / 50)
+            entity.get(TransformComponent).pos.y = lerp(entity.get(TransformComponent).pos.y, state.y, elapsedMs / 50)
+        }
+    }
+}
