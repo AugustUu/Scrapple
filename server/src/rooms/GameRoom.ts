72b803009584e7c073892eacbca5f0d0d74a0b98
tschmidt8320 <98917011+tschmidt8320@users.noreply.github.com>
2025-01-21 14:41:05 -0800

    added reload on left click

--- server/src/rooms/GameRoom.ts
+++ server/src/rooms/GameRoom.ts
@@ -34,17 +34,25 @@ export class GameRoom extends Room<State> {
             let gunInfo = Guns.get(player.gun.gunID)
             //console.log(JSON.stringify(player.gun))
 
-            if (player.gun.ammo > 0 && (player.gun.lastTimeShot + player.gun.fireDelay) < Date.now() && (player.gun.lastTimeReloaded + player.gun.reloadDelay) < Date.now()) {
-                player.gun.ammo -= 1;
+            if ((player.gun.lastTimeShot + player.gun.fireDelay) < Date.now() && (player.gun.lastTimeReloaded + player.gun.reloadDelay) < Date.now()) {
+                if(player.gun.ammo > 0){
+                    player.gun.ammo -= 1;
 
-                for (let i = 0; i < player.gun.bulletsPerShot; i++) {
-                    
-                    let angle = message.angle + (getRandomNumber(gunInfo.spread * -1, gunInfo.spread + 1) * (Math.PI / 180))
-                    this.state.bullets.set(randomBytes(16).toString('hex'), new Bullet(player.position.x, player.position.y, angle, client.id))
-                    console.log(message.angle)
+                    for (let i = 0; i < player.gun.bulletsPerShot; i++) {
+                        
+                        let angle = message.angle + (getRandomNumber(gunInfo.spread * -1, gunInfo.spread + 1) * (Math.PI / 180))
+                        this.state.bullets.set(randomBytes(16).toString('hex'), new Bullet(player.position.x, player.position.y, angle, client.id))
+                        console.log(message.angle)
+                    }
+
+                    player.gun.lastTimeShot = Date.now()
                 }
-
-                player.gun.lastTimeShot = Date.now()
+                else{
+                    if ((player.gun.lastTimeReloaded + player.gun.reloadDelay) < Date.now()) {
+                        player.gun.lastTimeReloaded = Date.now();
+                        player.gun.ammo = gunInfo.magSize
+                    }
+                }
 
             }
         })
@@ -52,11 +60,6 @@ export class GameRoom extends Room<State> {
         this.onMessage(C2SPacket.Reload, (client, message) => {
             let player = this.state.players.get(client.sessionId)
             let gunInfo = Guns.get(player.gun.gunID)
-
-            if ((player.gun.lastTimeReloaded + player.gun.reloadDelay) < Date.now()) {
-                player.gun.lastTimeReloaded = Date.now();
-                player.gun.ammo = gunInfo.magSize
-            }
         })
 
         this.onMessage(C2SPacket.Grapple, (client, message) => {
