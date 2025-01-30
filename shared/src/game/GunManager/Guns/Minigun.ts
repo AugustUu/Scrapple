f5eceb285d9df06e522a6f131b46eb4fd68e86a8
sfilner7055 <98917115+sfilner7055@users.noreply.github.com>
2025-01-30 13:24:54 -0800

    damage is now based on gun of who shot you
    also changed some values of guns

--- shared/src/game/GunManager/Guns/Minigun.ts
+++ shared/src/game/GunManager/Guns/Minigun.ts
@@ -5,6 +5,6 @@ import { registerGun } from "../GunManager";
 export class Minigun extends Gun {
 
     constructor(){
-        super("minigun", 1, 0.1, 3, 50, 1, 1, true, 20)
+        super("minigun", 3, 0.1, 3, 50, 1, 4, true, 20)
     }
 }
