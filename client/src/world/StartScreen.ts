f94a79e151db81aedee2b5fc280a3dc4e4951659
sfilner7055 <98917115+sfilner7055@users.noreply.github.com>
2025-02-11 13:49:22 -0800

    working on putting upgrade options in startscreen

--- client/src/world/StartScreen.ts
+++ client/src/world/StartScreen.ts
@@ -1,15 +1,21 @@
 import { Engine, Scene, SceneActivationContext } from "excalibur";
 import { Gun } from "shared/src/game/GunManager/Gun";
 import { Guns } from "shared/src/game/GunManager/GunManager"
-
+import { Game, LocalPlayerInstance } from "./Game";
 import { Networking } from "../networking/Networking";
 import { C2SPacket } from "shared/src/networking/Packet";
+import { LocalPlayer } from "../game/LocalPlayer";
+import { Inventory } from "../game/Inventory";
 
 export class StartScreen extends Scene {
     private rootElement!: HTMLElement;
     private gun1Button!: HTMLElement;
     private gun2Button!: HTMLElement;
     private gun3Button!: HTMLElement;
+    private upgrade1Button!: HTMLElement;
+    private upgrade2Button!: HTMLElement;
+    private upgrade3Button!: HTMLElement;
+
 
     private startButton!: HTMLElement;
 
@@ -17,18 +23,22 @@ export class StartScreen extends Scene {
 
     public onInitialize(engine: Engine) {
         this.rootElement = document.getElementById('startscreen')!;
+
         this.gun1Button = document.getElementById('gun1Button')!;
         this.gun2Button = document.getElementById('gun2Button')!;
         this.gun3Button = document.getElementById('gun3Button')!;
+        this.upgrade1Button = document.getElementById('upgrade1Button')!;
+        this.upgrade2Button = document.getElementById('upgrade2Button')!;
+        this.upgrade3Button = document.getElementById('upgrade3Button')!;
 
-        let array = Array.from(Guns.keys())
+
+        //set gun buttons to be guns
+
         let gunArray = new Array
-
         for(let i = 0; i < 6; i++){
-            gunArray[i] = array[i]
+            gunArray[i] = Array.from(Guns.keys())[i]
         }
 
-        
         let gunNum = Math.floor(Math.random() * gunArray.length)
         this.gun1Button.innerHTML = gunArray[gunNum]
         gunArray.splice(gunNum, 1)
@@ -40,6 +50,15 @@ export class StartScreen extends Scene {
         gunNum = Math.floor(Math.random() * (gunArray.length))
         this.gun3Button.innerHTML = gunArray[gunNum]
         gunArray.splice(gunNum, 1)
+
+        //set ugprade buttons to have upgrades
+        //LocalPlayerInstance.inventory.updateUsableUpgrades()
+        let upgradeArray = new Array
+
+
+
+
+        
         
         
         this.playerList = document.getElementById('playerList')!;
