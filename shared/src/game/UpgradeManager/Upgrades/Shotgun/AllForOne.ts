84e8262830f920e54d1b270e04a502bbc5cdab05
sfilner7055 <98917115+sfilner7055@users.noreply.github.com>
2025-04-29 12:45:09 -0700

    workin on stuff

--- shared/src/game/UpgradeManager/Upgrades/Shotgun/AllForOne.ts
+++ shared/src/game/UpgradeManager/Upgrades/Shotgun/AllForOne.ts
@@ -11,9 +11,9 @@ export class AllForOne extends Upgrade {
     serverOnGunConstructed(level: number, gun: GunState) {
         gun.bulletSize *= (gun.bulletsPerShot/2)
         gun.bulletSpeedMultiplier /= (gun.bulletsPerShot/4)
-        gun.damage *=  (gun.bulletsPerShot/4)
+        gun.damage *=  (gun.bulletsPerShot)
         
-        gun.bulletsPerShot = 1  
+        gun.bulletsPerShot = 1
         gun.reloadDelay *= 2
 
     }
