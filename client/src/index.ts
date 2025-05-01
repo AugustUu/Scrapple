8e379714eab0fc1a2b261f97292424f8e9a884fa
AugustUu <98927957+AugustUu@users.noreply.github.com>
2025-05-01 12:50:00 -0700

    some tweaks

--- client/src/index.ts
+++ client/src/index.ts
@@ -16,7 +16,7 @@ export const engine = new Engine({
     fixedUpdateFps: 120,
     width: 1920,
     height: 1080,
-    displayMode: DisplayMode.FitScreenAndFill,
+    displayMode: DisplayMode.FitScreenAndZoom,
     canvasElementId: 'game',
     antialiasing: {
         pixelArtSampler: true, // turns on the sub-pixel shader for pixel art
