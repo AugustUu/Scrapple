918a998ff8505cde8b3a74676c5eda5e5c8bf479
sfilner7055 <98917115+sfilner7055@users.noreply.github.com>
2025-05-15 13:06:35 -0700

    birdshot and buckshot and also some upgrade descriptions

--- shared/src/game/UpgradeManager/Upgrades/Shotgun/BirdShot.ts
+++ shared/src/game/UpgradeManager/Upgrades/Shotgun/BirdShot.ts
@@ -1,9 +1,17 @@
 import { Upgrade } from "../../Upgrade"
 import { registerUpgrade } from "../../UpgradeManager"
+import { GunState } from "../../../../../../server/src/State"
 
 @registerUpgrade
 export class BirdShot extends Upgrade {
     constructor() {
-        super("Bird Shot", 1, null, "Shotgun", "If your bullets are smaller you can pack more in a shell (UNIMPLEMENTED)")
+        super("Bird Shot", 1, null, "Shotgun", "If your bullets are smaller you can pack more in a shell")
+        //more bullets more spread less damage
+    }
+
+    serverOnGunConstructed(level: number, gun: GunState) {
+        gun.bulletsPerShot += 3
+        gun.damage *= 0.8
+        gun.spread *= 1.25
     }
 }
--- shared/src/game/UpgradeManager/Upgrades/Shotgun/BuckShot.ts
+++ shared/src/game/UpgradeManager/Upgrades/Shotgun/BuckShot.ts
@@ -1,9 +1,18 @@
 import { Upgrade } from "../../Upgrade"
 import { registerUpgrade } from "../../UpgradeManager"
+import { GunState } from "../../../../../../server/src/State"
 
 @registerUpgrade
 export class BuckShot extends Upgrade {
     constructor() {
-        super("Buck Shot", 1, null, "Shotgun", "Larger bullets means you can only fit so many in one shell (UNIMPLEMENTED)")
+        super("Buck Shot", 1, null, "Shotgun", "Larger bullets means you can only fit so many in one shell")
+        //less spread less bullets more damage
+        
+    }
+
+    serverOnGunConstructed(level: number, gun: GunState) {
+        gun.bulletsPerShot -= 2
+        gun.damage *= 1.25
+        gun.spread *= 0.75
     }
 }
